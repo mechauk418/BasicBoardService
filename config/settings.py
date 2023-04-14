4ea318cba603f88291cf15d98f9d8b1a85e199a8
mechauk418 <mechauk418@gmail.com>
2023-04-14 19:09:08 +0900

    cors update

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    "corsheaders",
 
     'rest_framework',
     'rest_framework.authtoken',
